32f6e24573020d71ff919df116c366415cbd336c
Dell <Dell@ADMINIS-7RJLIPQ>
2024-03-18 22:21:05 -0600

    Inicio de Comentarios

---
+++ Progra1Grupo42024/src/main/java/com/mycompany/progra1grupo42024/Progra1Grupo42024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progra1grupo42024;
+
+/**
+ *
+ * @author Dell
+ */
+public class Progra1Grupo42024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
