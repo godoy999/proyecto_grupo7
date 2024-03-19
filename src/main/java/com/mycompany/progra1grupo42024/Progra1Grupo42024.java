f745054033df8eb1ec45788f01825bddb28ef852
OscarEduardoOsorio <163203284+OscarEduardoOsorio@users.noreply.github.com>
2024-03-18 22:32:06 -0600

    cambios a prueba

---
+++ src/main/java/com/mycompany/progra1grupo42024/Progra1Grupo42024.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progra1grupo42024;
+
+/**
+ *
+ * @author Dell
+ */
+public class Progra1Grupo42024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello comrades");
+        System.out.println("hola compañeros haciendo prueba");
+    }
+}
