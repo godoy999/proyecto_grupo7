b1cd6c581c5a0880426ce4cf74722e074d017ffa
Melvin <godoymelvin0@gmail.com>
2024-08-21 21:32:20 -0600

    soy carlos hice un cambio

---
+++ proyecto_grupo7/src/main/java/com/mycompany/proyecto_grupo7/Proyecto_grupo7.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_grupo7;
+
+
+public class Proyecto_grupo7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
